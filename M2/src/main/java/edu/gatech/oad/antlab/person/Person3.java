f7fd98b4ff985d5d6a0a59cd612deebec4ffa1e6
Dieci, Estella <estella@dieci-mathm1507.local>
2016-01-28 18:08:23 -0500

    Added method to Person3.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -45,7 +45,7 @@ public class Person3 {
 	private String calc(String input) {
 	  //Person 3 put your implementation here
 	  String s = "";
-	  for (int i = input.length - 1; i >= 0; i++) {
+	  for (int i = input.length() - 1; i >= 0; i++) {
 	  	s += input.charAt(i);
 	  }
 	  return s;
