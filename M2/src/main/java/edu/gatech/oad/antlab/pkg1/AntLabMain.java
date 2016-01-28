9a8231fb272c9d06687b9212ece385763f1fcc6b
Thompson, William Barrett <wthompson40@gatech.edu>
2016-01-28 18:30:03 -0500

    Name and GTID to AntLab

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -61,8 +61,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p1.toString("lallen35");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("P2");
-		  toPrint += p2.toString("gburdell2");
+		  Person2 p2 = new Person2("Will Thompson");
+		  toPrint += p2.toString("wthompson40");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Estella Dieci");
