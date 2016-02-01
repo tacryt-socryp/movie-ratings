50e8f11b6169c4889c1556609c9d05858de08067
Jeremy Paul <jeremypaul@gatech.edu>
2016-01-31 20:55:59 -0500

    GTID and name in AntLabMain
    changed person 5 gtid and name

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("mmyers39");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Jeremy Paul");
+                  toPrint += p5.toString("jpaul33");
 
         System.out.println(toPrint);
 
