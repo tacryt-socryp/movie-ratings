748dc1cc69e67d110a007618006bdc02da181433
Mitch Myers <mmyers39@gatech.edu>
2016-01-31 20:42:52 -0500

    Changes to Person 4
    Completed the calc method for Person 4.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Mitch Myers
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,12 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+        String one_greater = "";
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
+            one_greater += (char) (character + 1);
+        }
+        return one_greater;
     }
     
     /**
