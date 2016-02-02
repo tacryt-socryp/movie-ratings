f6a9b45a00047e47108c49a13232e2ee32058574
Thompson, William Barrett <wthompson40@gatech.edu>
2016-02-01 20:41:38 -0500

    Fixed person2.java
    Problem with looping through initially

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -31,21 +31,20 @@ public class Person2 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
+	private static String calc(String input) {
 	  //Person 2 put your implementation here
 	  Random randy = new Random();
 		String clone = input;
 		String toReturn = "";
 		for (int i = 0; i < input.length(); i++) {
-			int temp = randy.nextInt(input.length() + 1);
-			System.out.println(temp);
+			int temp = randy.nextInt(input.length() - i);
 			toReturn += clone.charAt(temp);
 			if (temp == 0) {
 				clone = input.substring(1, input.length() - 1);
 			} else if (temp == input.length() - 1) {
 				clone = input.substring(0, input.length() - 2);
 			} else {
-			clone = input.substring(0, temp -1) + input.substring(temp + 1, input.length() - 1);
+				clone = input.substring(0, temp -1) + input.substring(temp + 1, input.length() - 1);
 			}
 		}
 	  return toReturn;
