5c249dcd14b85419fbc53143a7f4f3d3b1eea271
Logan Allen <loganallc@gmail.com>
2016-03-16 14:44:04 -0400

    Finished merging :(

--- RottenTomatoes/app/src/main/java/models/UserModel.java
+++ RottenTomatoes/app/src/main/java/models/UserModel.java
@@ -120,25 +120,9 @@ public class UserModel implements Parcelable {
         username = user;
         password = pass;
         profile = p;
-<<<<<<< HEAD
         isAdmin = admin;
         isActive = true;
     }
-    /*
-    public UserModel(String user) {
-    }
-
-    public UserModel(int a) {
-    }
-
-    public String toString() {
-        return username + "\n" + status;
-=======
-        isAdmin = isAdmin;
-        isActive = true;
->>>>>>> origin/master
-    }
-    */
 
     /**
      * empty init for Jackson compatibility
