de105f4052b46634ac8433749769d02d2ebeb855
Logan Allen <loganallc@gmail.com>
2016-04-04 19:33:37 -0400

    Added an example of a JUnit test

---
+++ RottenTomatoes/app/src/test/java/teamfour/com/rottentomatoes/RecommendationTest.java
@@ -0,0 +1,15 @@
+package teamfour.com.rottentomatoes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class RecommendationTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
